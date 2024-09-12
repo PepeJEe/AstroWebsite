89c1df80199a7210d26c24a4a11665e5b4ca80fe
PepeJEe <83735018+PepeJEe@users.noreply.github.com>
2024-09-12 21:41:00 +0300

    testing

---
+++ src/fetchastrodata.js
@@ -0,0 +1,20 @@
+/*document.addEventListener("DOMContentLoaded", onDOMLoaded);
+
+async function onDOMLoaded() {
+    const apiKey = "0af6e807b4a55389d2580d2ea9bedf982d70f649";
+    const apiSecret = "6d516a217c147eeb5438d613b1fcc148276dd274";
+
+    try {
+        const response = `https://www.astrobin.com/api/v1/objects/?&api_key=${apiKey}&api_secret=${apiSecret}&format=json`;
+        if(response.ok)
+        {
+            console.log("SUCCESS");
+        }
+        else
+        {
+            console.error("Fetch failed with status:", response.status);
+        }
+    } catch (error) {
+        console.log("ERROR", error);
+    }
+}*/
